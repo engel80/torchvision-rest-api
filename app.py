bccbaaf8e524a1e059a6afbd2a948da272e5ac8b
engel80 <prettyhip@gmail.com>
2022-04-06 15:59:35 +0900

    .

--- app.py
+++ app.py
@@ -48,9 +48,13 @@ def ping():
     return 'OK'
 
 @app.route("/cuda")
-def ping():
-    return 'torch.cuda.is_available:'+ str(torch.cuda.is_available())
-    
+def cuda():
+    if torch.cuda.is_available():
+        return "'torch.cuda.is_available': {}, 'torch.cuda.device_coun': {}, 'torch.cuda.current_device': {}, 'torch.cuda.get_device_name': {}".format(str(torch.cuda.is_available()), str(torch.cuda.device_count()), str(torch.cuda.current_device()), torch.cuda.get_device_name(torch.cuda.current_device()))
+    else:
+        return "'torch.cuda.is_available': {}".format(str(False))
+        
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=False)
+    # app.run(host='0.0.0.0', port=8000, debug=False)
+    app.run(host='0.0.0.0', port=9000, debug=True)
     # app.run()
--- test.sh
+++ test.sh
@@ -7,3 +7,9 @@ docker build -t torchvision .
 docker tag torchvision:latest 681747700094.dkr.ecr.ap-northeast-2.amazonaws.com/torchvision:latest
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 681747700094.dkr.ecr.ap-northeast-2.amazonaws.com
 docker push 681747700094.dkr.ecr.ap-northeast-2.amazonaws.com/torchvision:latest
+
+
+
+vision-api.
+
+curl -X POST -F file=@cat_pic.jpeg https://vision-api.octankshop.com/predict
