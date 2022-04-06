e39d7116c2bb4f8b8395443d92d49aaa1eb6aaa9
engel80 <prettyhip@gmail.com>
2022-04-06 12:19:57 +0900

    .

---
+++ build.sh
@@ -0,0 +1,8 @@
+
+docker build -t torchvision .
+
+docker tag torchvision:latest 681747700094.dkr.ecr.ap-northeast-2.amazonaws.com/torchvision:latest
+
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 681747700094.dkr.ecr.ap-northeast-2.amazonaws.com
+
+docker push 681747700094.dkr.ecr.ap-northeast-2.amazonaws.com/torchvision:latest
