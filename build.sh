f136a2615c221eb049aab20326c7c3146aeae6e0
engel80 <prettyhip@gmail.com>
2022-04-06 12:22:29 +0900

    Revert "."
    This reverts commit e39d7116c2bb4f8b8395443d92d49aaa1eb6aaa9.

--- build.sh
+++
@@ -1,8 +0,0 @@
-
-docker build -t torchvision .
-
-docker tag torchvision:latest 681747700094.dkr.ecr.ap-northeast-2.amazonaws.com/torchvision:latest
-
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 681747700094.dkr.ecr.ap-northeast-2.amazonaws.com
-
-docker push 681747700094.dkr.ecr.ap-northeast-2.amazonaws.com/torchvision:latest
